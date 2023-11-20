f2e279f62417c8a6aa513efb76903e80345ccb07
Dmitrii Malykh <malyh92@mail.ru>
2023-11-20 21:48:28 +0400

    feat: функционал вывода ингредиентов из массива

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,8 +1,39 @@
 import React from 'react';
+import {Tab} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './burger-ingredients.module.css'
+import IngredientCategory from "../ingredient-category/ingredient-category";
 
 function BurgerIngredients(props) {
+    const [current, setCurrent] = React.useState('one');
+
+    // Категории ингредиентов
+    const categories = [
+        {value: 'one', label: 'Булки', type: 'bun'},
+        {value: 'two', label: 'Соусы', type: 'sauce'},
+        {value: 'three', label: 'Начинки', type: 'main'},
+    ];
+
     return (
-        <div>Ингридиенты</div>
+        <>
+            <div className={styles.tab}>
+                {categories.map((category) => (
+                    <Tab
+                        key={category.value}
+                        value={category.value}
+                        active={current === category.value}
+                        onClick={setCurrent}
+                    >
+                        {category.label}
+                    </Tab>
+                ))}
+            </div>
+
+            <div className={styles.ingredients}>
+                {categories.map((category) => (
+                    <IngredientCategory {...category}/>
+                ))}
+            </div>
+        </>
     );
 }
 
