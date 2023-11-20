bee1fd828427d8eddf708d6364f577cfbb608957
Dmitrii Malykh <malyh92@mail.ru>
2023-11-20 16:45:57 +0400

    feat: функциональность AppHeader

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import styles from './app-header.module.css'
-import {Logo} from "@ya.praktikum/react-developer-burger-ui-components";
+import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
 
 const AppHeader = () => {
     return (
-        <header className={styles.header}>
-            <Logo/>
+        <header className={styles.header + ` pt-4 pb-4`}>
+            <nav className={styles.nav}>
+                <div className={`pl-5 pr-5`}>
+                    <a href="#" className={styles.link + ' ' + styles.link_active}>
+                        <div className={styles.icon}><BurgerIcon type="primary"/></div>
+                        <p className={`text text_type_main-default ml-2`}>Конструктор</p>
+                    </a>
+                </div>
+                <div className={`pl-5 pr-5 ml-2`}>
+                    <a href="#" className={styles.link}>
+                        <div className={styles.icon}><ListIcon type="secondary"/></div>
+                        <p className={`text text_type_main-default ml-2`}>Лента Заказов</p>
+                    </a>
+                </div>
+                <div className={styles.logo}>
+                    <Logo/>
+                </div>
+                <div className={styles.lk + ` pl-5 pr-5`}>
+                    <a href="#" className={styles.link}>
+                        <div className={styles.icon}><ProfileIcon type="secondary"/></div>
+                        <p className={`text text_type_main-default ml-2`}>Личный кабинет</p>
+                    </a>
+                </div>
+            </nav>
         </header>
     );
 };
