f0c4d219ecca82b45cb9bccf76793a6e532aa464
Dmitrii Malykh <malyh92@mail.ru>
2023-11-21 13:18:45 +0400

    ref: удалены самописные стили скролбаров

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -3,70 +3,69 @@ import {ConstructorElement} from "@ya.praktikum/react-developer-burger-ui-compon
 import styles from './burger-constructor.module.css'
 
 function BurgerConstructor(props) {
-    return (
-        <div className={styles.components + ` mt-25 ml-8`}>
-            <ConstructorElement
-                type="top"
-                isLocked={true}
-                text="Краторная булка N-200i (верх)"
-                price={200}
-                thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-            />
-            <div className={styles.ingredients}>
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+  return (
+    <div className={styles.components + ` mt-25 ml-8`}>
+      <ConstructorElement
+        type="top"
+        isLocked={true}
+        text="Краторная булка N-200i (верх)"
+        price={200}
+        thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+      />
+      <div className={styles.ingredients + ' custom-scroll'}>
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+        <ConstructorElement
+          text="Краторная булка N-200i (верх)"
+          price={50}
+          thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-                />
-                <ConstructorElement
-                    text="Краторная булка N-200i (верх)"
-                    price={50}
-                    thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
+        />
+      </div>
 
-                />
-            </div>
+      <ConstructorElement
+        type="bottom"
+        isLocked={true}
+        text="Краторная булка N-200i (низ)"
+        price={200}
+        thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
 
-            <ConstructorElement
-                type="bottom"
-                isLocked={true}
-                text="Краторная булка N-200i (низ)"
-                price={200}
-                thumbnail="https://code.s3.yandex.net/react/code/bun-02-mobile.png"
-
-            />
-        </div>
-    );
+      />
+    </div>
+  );
 }
 
 export default BurgerConstructor;
--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -4,37 +4,37 @@ import styles from './burger-ingredients.module.css'
 import IngredientCategory from "../ingredient-category/ingredient-category";
 
 function BurgerIngredients(props) {
-    const [current, setCurrent] = React.useState('one');
+  const [current, setCurrent] = React.useState('one');
 
-    // Категории ингредиентов
-    const categories = [
-        {value: 'one', label: 'Булки', type: 'bun'},
-        {value: 'two', label: 'Соусы', type: 'sauce'},
-        {value: 'three', label: 'Начинки', type: 'main'},
-    ];
+  // Категории ингредиентов
+  const categories = [
+    {value: 'one', label: 'Булки', type: 'bun'},
+    {value: 'two', label: 'Соусы', type: 'sauce'},
+    {value: 'three', label: 'Начинки', type: 'main'},
+  ];
 
-    return (
-        <>
-            <div className={styles.tab}>
-                {categories.map((category) => (
-                    <Tab
-                        key={category.value}
-                        value={category.value}
-                        active={current === category.value}
-                        onClick={setCurrent}
-                    >
-                        {category.label}
-                    </Tab>
-                ))}
-            </div>
+  return (
+    <>
+      <div className={styles.tab}>
+        {categories.map((category) => (
+          <Tab
+            key={category.value}
+            value={category.value}
+            active={current === category.value}
+            onClick={setCurrent}
+          >
+            {category.label}
+          </Tab>
+        ))}
+      </div>
 
-            <div className={styles.ingredients}>
-                {categories.map((category) => (
-                    <IngredientCategory {...category}/>
-                ))}
-            </div>
-        </>
-    );
+      <div className={styles.ingredients + ' custom-scroll'}>
+        {categories.map((category) => (
+          <IngredientCategory {...category}/>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default BurgerIngredients;
