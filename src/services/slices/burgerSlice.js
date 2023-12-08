f49985fdb8fc70bdc7aece9ebf4c08b1d45906f2
Dmitrii Malykh <malyh92@mail.ru>
2023-12-08 22:29:15 +0400

    fix: удалена предопределенная булочка  в burgerSlice.js

--- src/services/slices/burgerSlice.js
+++ src/services/slices/burgerSlice.js
@@ -2,20 +2,7 @@ import {createSlice} from '@reduxjs/toolkit';
 import {v4 as uuidv4} from 'uuid';
 
 const initialState = {
-  bun: {
-    "_id": "60666c42cc7b410027a1a9b1",
-    "name": "Краторная булка N-200i",
-    "type": "bun",
-    "proteins": 80,
-    "fat": 24,
-    "carbohydrates": 53,
-    "calories": 420,
-    "price": 20,
-    "image": "https://code.s3.yandex.net/react/code/bun-02.png",
-    "image_mobile": "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
-    "image_large": "https://code.s3.yandex.net/react/code/bun-02-large.png",
-    "__v": 0
-  },
+  bun: null,
   ingredients: [],
 };
 
