ea01168bce88018b7a94fdbef048fb1c9cb8ccaf
izhddm <malyh92@mail.ru>
2023-12-23 04:05:03 +0400

    feat(header): навигация кликабельная, активная подсвечивается

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 import styles from './app-header.module.css'
 import {BurgerIcon, ListIcon, Logo, ProfileIcon} from "@ya.praktikum/react-developer-burger-ui-components";
+import {NavLink} from "react-router-dom";
 
 const AppHeader = () => {
   return (
     <header className={styles.header + ' pt-4 pb-4'}>
       <nav className={styles.nav}>
         <div className="pl-5 pr-5">
-          <a href="#" className={styles.link + ' text text_type_main-default'}>
-            <div className={styles.icon}><BurgerIcon type="primary"/></div>
-            <p className="text text_type_main-default ml-2">Конструктор</p>
-          </a>
+          <NavLink to={'/'} className={styles.link}>
+            {({isActive}) => (
+              <>
+                <div className={styles.icon}>
+                  <BurgerIcon type={isActive ? "primary" : "secondary"}/>
+                </div>
+                <p className={`text text_type_main-default ml-2 ${isActive ? '' : styles.inactive}`}>Конструктор</p>
+              </>
+            )}
+          </NavLink>
         </div>
         <div className="pl-5 pr-5 ml-2">
-          <a href="#" className={styles.link + ' text text_type_main-default text_color_inactive'}>
-            <div className={styles.icon}><ListIcon type="secondary"/></div>
-            <p className="text text_type_main-default ml-2">Лента Заказов</p>
-          </a>
+          <NavLink to={'/feed'} className={styles.link}>
+            {({isActive}) => (
+              <>
+                <div className={styles.icon}>
+                  <ListIcon type={isActive ? "primary" : "secondary"}/>
+                </div>
+                <p className={`text text_type_main-default ml-2 ${isActive ? '' : styles.inactive}`}>Лента Заказов</p>
+              </>
+            )}
+          </NavLink>
         </div>
         <div className={styles.logo}>
           <Logo/>
         </div>
         <div className={styles.lk + ' pl-5 pr-5'}>
-          <a href="#" className={styles.link + ' text text_type_main-default text_color_inactive'}>
-            <div className={styles.icon}><ProfileIcon type="secondary"/></div>
-            <p className="text text_type_main-default ml-2">Личный кабинет</p>
-          </a>
+          <NavLink to={'/profile'} className={styles.link}>
+            {({isActive}) => (
+              <>
+                <div className={styles.icon}>
+                  <ProfileIcon type={isActive ? "primary" : "secondary"}/>
+                </div>
+                <p className={`text text_type_main-default ml-2 ${isActive ? '' : styles.inactive}`}>Личный кабинет</p>
+              </>
+            )}
+          </NavLink>
         </div>
       </nav>
     </header>
