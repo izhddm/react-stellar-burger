710622dd353c965a573aa800368b7b0154d6426e
izhddm <malyh92@mail.ru>
2024-01-08 01:56:58 +0400

    fix: исправлена типизация для утилиты разбора jwt токена

--- src/utils/jwtUtils.ts
+++ src/utils/jwtUtils.ts
@@ -1,26 +1,32 @@
-function removeBearerPrefix(token: string) {
+type DecodedToken = {
+  header: Record<string, unknown>;
+  payload: Record<string, unknown>;
+  signature: string;
+};
+
+function removeBearerPrefix(token: string): string {
   // Удаление префикса "Bearer ", если он присутствует
   return token.startsWith('Bearer ') ? token.slice(7) : token;
 }
 
-function decodeToken(token: string) {
+function decodeToken(token: string): DecodedToken {
   try {
     const cleanedToken = removeBearerPrefix(token);
 
     const [header, payload, signature] = cleanedToken.split('.');
 
     // Декодирование заголовка и полезной нагрузки Base64
-    const decodedHeader = JSON.parse(atob(header));
-    const decodedPayload = JSON.parse(atob(payload));
+    const decodedHeader = JSON.parse(atob(header)) as Record<string, unknown>;
+    const decodedPayload = JSON.parse(atob(payload)) as Record<string, unknown>;
 
-    return {header: decodedHeader, payload: decodedPayload, signature};
+    return { header: decodedHeader, payload: decodedPayload, signature };
   } catch (error: unknown) {
     console.error('Ошибка при декодировании токена:', (error as Error).message);
     throw error;
   }
 }
 
-export function isJwtTokenValid(token: string) {
+export function isJwtTokenValid(token: string | null): boolean | undefined {
   try {
     if (!token) {
       return false;
@@ -28,7 +34,7 @@ export function isJwtTokenValid(token: string) {
 
     const decodedToken = decodeToken(token);
 
-    return decodedToken.payload && decodedToken.payload.exp * 1000 > Date.now();
+    return decodedToken.payload && (decodedToken.payload.exp as number) * 1000 > Date.now();
   } catch (error: unknown) {
     console.error('Ошибка при проверке токена:', (error as Error).message);
   }
