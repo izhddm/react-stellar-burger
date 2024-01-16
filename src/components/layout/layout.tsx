29cb557556ca04637b445bd1cfeb60281f208289
izhddm <malyh92@mail.ru>
2024-01-16 12:19:36 +0400

    fix: исправлена работа ESC если модальное окно не было открытым

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,12 @@
 import React, {FC} from 'react';
 import {Outlet} from "react-router-dom";
 import AppHeader from "../app-header/app-header";
-import Modal from "../modal/modal";
 
 const Layout: FC = () => {
   return (
     <>
       <AppHeader/>
       <Outlet/>
-      <Modal/>
     </>
   );
 }
