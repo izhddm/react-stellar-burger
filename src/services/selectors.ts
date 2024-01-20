31474dfa8a5fa986a4bddb227182c256c8a44dd0
izhddm <malyh92@mail.ru>
2024-01-21 01:30:43 +0400

    fix: не отображалась детальная информация об ингредиенте

--- src/services/selectors.ts
+++ src/services/selectors.ts
@@ -1,6 +1,5 @@
 import {TIngredient} from "../types/types";
 import {RootState} from "./store/store";
-import {useGetIngredientsQuery} from "./api/ingredient-api";
 
 export const getCountIngredientFromConstructor = (element: TIngredient) => (state: RootState) => {
   if (state.burger.bun && element._id === state.burger.bun._id) {
@@ -8,15 +7,4 @@ export const getCountIngredientFromConstructor = (element: TIngredient) => (stat
   }
 
   return state.burger.ingredients.reduce((acc, ingredient) => (ingredient._id === element._id ? acc + 1 : acc), 0);
-}
-
-// Возвращает ингредиент, либо из стейта, либо находит по id среди всех ингредиентов
-export const getDetailIngredient = (id: string | undefined) => (state: RootState) => {
-  if (state.modal.data) {
-    return state.modal.data as TIngredient;
-  }
-
-  const {data: ingredients} = useGetIngredientsQuery(); // Получения списка ингредиентов
-
-  return ingredients?.find((el) => el._id === id) ?? null;
 }
